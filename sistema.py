ebb8f8fc43f6990cc7f49f3e8b98317d9458f52c
joaquinurquiza-afk <joaquin.urquiza@alumnos.udemm.edu.ar>
2026-09-18 15:10:48 -0300

    Create sistema.py

---
+++ sistema.py
@@ -0,0 +1,7 @@
+#Sistema de detencion
+
+def sistem(sujeto):
+    if sujeto == "dead": #Si la distancia deciende a 0, el sujeto muere y detiene el programa
+        return True
+    else:
+        return False
